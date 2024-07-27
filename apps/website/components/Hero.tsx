e53d39af63633cb3d986dbb456cf7471c7b317be
gopalverma1303 <vgopaldps@gmail.com>
2024-07-27 16:08:32 +0530

    fix: hero made responsive flex-col after lg:

--- apps/website/components/Hero.tsx
+++ apps/website/components/Hero.tsx
@@ -9,7 +9,7 @@ import BentoGrid from "./BentoGrid";
 
 const ShuffleHero = () => {
   return (
-    <section className="w-full px-8 py-12  grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto ">
+    <section className="w-full px-8 py-12  grid grid-cols-1 lg:grid-cols-2 items-center gap-8 max-w-6xl mx-auto ">
       <div>
         <span className="block mb-4 text-xs md:text-sm text-[#5271ff] font-medium">
           Effortless UI Development with{" "}
