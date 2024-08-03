94f725ae46a3a9a3be5375b465c77809a53d45e3
gopalverma1303 <vgopaldps@gmail.com>
2024-08-03 10:39:04 +0530

    feat: template: init spw

---
+++ apps/templates/single-page-portfolio/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main: React.FC = () => {
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center gap-5">
+      <p>Single Page Portfolio using Nextra + Next.js + Tailwind CSS</p>
+      <p className="text-xs font-bold text-yellow-400">
+        🚧 Under Construction 🚧
+      </p>
+    </div>
+  );
+};
+
+export default Main;
